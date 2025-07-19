6bf88c60fe604ad96f208963e7f4b643ac53c9ce
SomRawat <76642632+SomRawat@users.noreply.github.com>
2025-07-19 18:14:51 +0530

    Refactor

---
+++ src/main/java/org/bluecollar/bluecollar/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package org.bluecollar.bluecollar.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
