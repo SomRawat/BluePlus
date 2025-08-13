325b28b8a0473d66f09892d01c7ef47bc8f1cc6f
SomRawat <76642632+SomRawat@users.noreply.github.com>
2025-08-13 18:08:12 +0530

    twillio fix

--- src/main/java/org/bluecollar/bluecollar/common/service/TwilioService.java
+++ src/main/java/org/bluecollar/bluecollar/common/service/TwilioService.java
@@ -1,38 +1,48 @@
 package org.bluecollar.bluecollar.common.service;
 
 import com.twilio.Twilio;
+import com.twilio.exception.ApiException;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TwilioService {
-    
+
+    private static final Logger logger = LoggerFactory.getLogger(TwilioService.class);
+
     @Value("${twilio.account.sid}")
     private String accountSid;
-    
+
     @Value("${twilio.auth.token}")
     private String authToken;
-    
+
     @Value("${twilio.phone.number:+12695570572}")
     private String fromPhoneNumber;
-    
+
+    @PostConstruct
+    public void initTwilio() {
+        Twilio.init(accountSid, authToken);
+        logger.info("Twilio service initialized.");
+    }
+
     public void sendSms(String toPhoneNumber, String messageBody) {
         try {
-            Twilio.init(accountSid, authToken);
-            
             Message message = Message.creator(
                     new PhoneNumber(toPhoneNumber),
                     new PhoneNumber(fromPhoneNumber),
                     messageBody
             ).create();
-            
-            System.out.println("SMS sent successfully. SID: " + message.getSid());
+
+            logger.info("SMS sent successfully to {}. SID: {}", toPhoneNumber, message.getSid());
+        } catch (ApiException e) {
+            logger.error("Twilio API Error sending SMS to {}: {} - Code: {}. More info: {}", toPhoneNumber, e.getMessage(), e.getCode(), e.getMoreInfo(), e);
         } catch (Exception e) {
-            System.err.println("Twilio SMS Error: " + e.getMessage());
-            System.out.println("OTP (SMS failed): " + messageBody);
-            // Don't throw exception, just log for development
+            logger.error("An unexpected error occurred when sending SMS to {}.", toPhoneNumber, e);
         }
     }
 }
