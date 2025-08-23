20d1cb0abdddf14b38917e80904fd82951039d7f
SomRawat <76642632+SomRawat@users.noreply.github.com>
2025-08-23 13:53:56 +0530

    refactor

--- src/main/java/org/bluecollar/bluecollar/admin/controller/AdminCouponController.java
+++ src/main/java/org/bluecollar/bluecollar/admin/controller/AdminCouponController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RequestMapping("/api/admin/deals")
+@RestController()
 public class AdminCouponController {
 
     private final CouponService couponService;
